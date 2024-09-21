c6242007935c5cdd881aa7c87bb73f08ded5ff86
Kay Disu <olukayode.disu77@gmail.com>
2024-09-21 09:03:50 -0600

    increment scrip update

--- app/script.groovy
+++ app/script.groovy
@@ -1,11 +1,9 @@
 def increment() {
-    dir("app") {
         sh "npm version minor"
         def packageJson = readJSON file: 'package.json'
         def version = packageJson.version
         env.IMAGE_NAME = "$version-$BUILD_NUMBER"
     }
-}
 def runTest() {
     dir("app") {
         sh "npm install"
