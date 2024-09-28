acd765f6baed5b4a571adac0c47d21c4512b38d6
Kay Disu <olukayode.disu77@gmail.com>
2024-09-28 11:21:50 -0600

    library and groovy configuration

--- app/script.groovy
+++ app/script.groovy
@@ -1,20 +1,6 @@
-def increment() {
-        sh "npm version minor"
-        def packageJson = readJSON file: 'package.json'
-        def version = packageJson.version
-        env.IMAGE_NAME = "$version-$BUILD_NUMBER"
-    }
-def runTest() {
-        sh "npm install"
-        sh "npm run test"
-    }
-def buildPushImage() {
-    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-        sh "docker build -t ebony9ja/nodes-twn-repo:${IMAGE_NAME} ./app"
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push ebony9ja/nodes-twn-repo:${IMAGE_NAME}"
-    }
-}
+
+
+
 def VersionUpdateCommit() {
     withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'git config --global user.email "kaydisu@gmail.com"'
